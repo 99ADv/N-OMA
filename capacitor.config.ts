2c2b7daa40bed7e113bff6e294a1b420535d186b
Jairo Rohatan <jarohatan@gmail.com>
2022-01-05 08:13:17 -0500

    add resources and script utils

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'N-OMA',
+  appName: 'OTRS-chat',
   webDir: 'www',
   bundledWebRuntime: false
 };
